0be321026ef20bca4227d3e096831a613e665b0b
mlproject14 <135835329+mlproject14@users.noreply.github.com>
2023-06-22 10:03:47 +0600

    Update aa.py

--- aa.py
+++ aa.py
@@ -1,19 +1,20 @@
 import streamlit as st
 import pytube
+import os
 
 def download_youtube_video(url):
     try:
         youtube = pytube.YouTube(url)
         video = youtube.streams.get_highest_resolution()
         video.download()
-        st.success("Download completed successfully!")
+        return video.title
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
 
 # Streamlit app
 def main():
     st.title("YouTube Video Downloader")
-    st.write("Enter the YouTube video URL and click the 'Download' button to download the video.")
+    st.write("Enter the YouTube video URL and click the 'Download' button to get the download link.")
 
     # Input field for the YouTube URL
     url = st.text_input("Enter YouTube URL")
@@ -22,7 +23,12 @@ def main():
     if st.button("Download"):
         if url:
             st.write("Downloading...")
-            download_youtube_video(url)
+            filename = download_youtube_video(url)
+            if filename:
+                file_path = os.path.join(os.getcwd(), filename + ".mp4")
+                st.success("Download completed successfully!")
+                st.write("Click the link below to download:")
+                st.markdown(f"[Download {filename}](./{filename}.mp4)")
         else:
             st.warning("Please enter a valid YouTube URL.")
 
